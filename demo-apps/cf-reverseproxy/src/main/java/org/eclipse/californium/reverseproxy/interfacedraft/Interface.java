6ececd493d0cd73d13c26ff0d8386ee4669e6321
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2016-05-12 17:45:45 +0200

    refactoring

---
+++ demo-apps/cf-reverseproxy/src/main/java/org/eclipse/californium/reverseproxy/interfacedraft/Interface.java
@@ -0,0 +1,9 @@
+package org.eclipse.californium.reverseproxy.interfacedraft;
+
+/**
+ * Created by jacko on 12/05/16.
+ */
+public final class Interface {
+    public static final String MINIMUM_PERIOD = "pmin";
+    public static final String MAXIMUM_PERIOD = "pmax";
+}
